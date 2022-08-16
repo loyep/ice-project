9ac109265073ac6aaea4f00f0c412197e001ee8b
Zack <lorenx@163.com>
2022-08-16 11:03:13 +0800

    feat: update ssr

---
+++ api/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+const app = express();
+const port = 3010;
+import * as ice from '../build/server/index.mjs'
+
+app.use(async (req, res, next) => {
+  //   if (req.path.match(/([0-9]+|umi)\.js/)) {
+  // await new Promise(resolve => setTimeout(resolve, 1000));
+  //   }
+  next();
+})
+
+// Logger middleware
+app.use((req, res, next) => {
+  console.info(`${req.method} ${req.url}`);
+  next();
+});
+
+// Umi SSR middleware
+app.use(async (req, res, next) => {
+  try {
+    console.log('req.url', req.path)
+    if (req.path.match(/(js|css)/)) {
+      next()
+      return
+    }
+    console.log(req.url)
+    const markup = await ice.renderToResponse({
+      req, res,
+    });
+    // next()
+  } catch (error) {
+    next(error)
+  }
+});
+
+// Umi static files (including SSG pages)
+app.use(express.static('build'));
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is now running at http://127.0.0.1:${port}/`);
+});
